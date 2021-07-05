95ce3718fc357adfa79818f1ccf3a934ef0d0cd9
Johannes Govaerts <johannes.govaerts@vsko.be>
2021-07-05 12:41:07 +0200

    adapted initialisation to new sri4node-vsko

--- js/sri-audit.js
+++ js/sri-audit.js
@@ -4,48 +4,73 @@
 
 const pMap = require('p-map');
 const uuid = require('uuid/v4');
+const { detailedDiff } = require("deep-object-diff");
 
-const doAudit = async function(tx, pluginConfig, sriRequest, elements, component, operation, mapping) {
+const doAudit = async function(tx, pluginConfig, sriRequest, elements, operation, mapping) {
   'use strict';
 
   await pMap(elements, async({ permalink, incoming: object, stored: stored }) => {
 
-    //TODO: don't use own regex -> put one in sri4node in utils
-    const typeString = permalink.match(/^\/(\/*.*)\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/);
-    const type = typeString[1].split('/').join('_').toUpperCase();
-    const doc = object;
-    if (pluginConfig.omitProperties && pluginConfig.omitProperties[type]) {
-      pluginConfig.omitProperties[type].forEach(property => {
-        delete doc[property];
-      });
-    }
-    const auditItem = {
-      key: uuid(),
-      person: sriRequest.userObject ? '/persons/' + sriRequest.userObject.uuid : '',
-      timestamp: (new Date()).toJSON(),
-      component: component,
-      operation: operation,
-      type: type,
-      resource: permalink,
-      document: object
-    };
+    // TODO: check if this old, never before checked in code haveDifferences is useful
+    // only send a new version if there are differences between stored and incoming objects
+    // if (haveDifferences(stored, object, mapping)) {
 
-    try {
-      await tx.any('INSERT INTO "versionsQueue" VALUES ($1, $2)', [auditItem.key, auditItem]);
-    }
-    catch (reason) {
-      console.error('[sri-audit] put version to database failed for resource: ' + permalink);
-      throw new sriRequest.SriError({ status: 500, errors: [{ code: 'version.queue.insert.failed', msg: 'Storage of new version in versionsQueue failed.' }] });
-    }
+        //TODO: don't use own regex -> put one in sri4node in utils
+        const typeString = permalink.match(/^\/(\/*.*)\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/);
+        const type = typeString[1].split('/').join('_').toUpperCase();
+        const doc = object;
+        if (pluginConfig.omitProperties && pluginConfig.omitProperties[type]) {
+          pluginConfig.omitProperties[type].forEach(property => {
+            delete doc[property];
+          });
+        }
+        const auditItem = {
+          key: uuid(),
+          person: sriRequest.userObject ? '/persons/' + sriRequest.userObject.uuid : '',
+          timestamp: (new Date()).toJSON(),
+          component: pluginConfig.component,
+          operation: operation,
+          type: type,
+          resource: permalink,
+          document: object
+        };
+
+        try {
+          await tx.any('INSERT INTO "versionsQueue" VALUES ($1, $2)', [auditItem.key, auditItem]);
+        }
+        catch (reason) {
+          console.error('[sri-audit] put version to database failed for resource: ' + permalink);
+          throw new sriRequest.SriError({ status: 500, errors: [{ code: 'version.queue.insert.failed', msg: 'Storage of new version in versionsQueue failed.' }] });
+        }
+    // }
   }, { concurrency: 1 });
 };
 
-module.exports = function(component, pluginConfig) {
-  'use strict';
+const haveDifferences = (stored, incoming, mapping) => {
+  
+  // force Date object for date-time incoming properties
+  Object.keys(mapping.map).forEach(mapKey => {
+    if (incoming[mapKey] && mapping.schema.properties[mapKey].format === 'date-time') {
+      incoming[mapKey] = new Date(incoming[mapKey]);
+    }
+  })
 
-  return {
+  const diff = detailedDiff(stored, incoming)
+  // console.log(diff)
+
+  // exclude properties starting with $$ (eg. $$meta)
+  const addedKeys = Object.keys(diff.added).filter(key => key.indexOf('$$') === -1)
+  const deletedKeys = Object.keys(diff.deleted).filter(key => key.indexOf('$$') === -1)
+  const updatedKeys = Object.keys(diff.updated).filter(key => key.indexOf('$$') === -1)
+  // console.log(addedKeys, deletedKeys, updatedKeys)
+
+  return updatedKeys.length > 0 || deletedKeys.length > 0 || addedKeys.length > 0;
+}
+
+
+module.exports = {
     install: async function(sriConfig, db) {
-
+      const pluginConfig = sriConfig.composedAuditPluginConfig;
       const create =
         `CREATE TABLE IF NOT EXISTS "versionsQueue"
            (
@@ -61,11 +86,9 @@ module.exports = function(component, pluginConfig) {
 
       sriConfig.resources.forEach(resource => {
         // audit functions should be LAST function in handler lists
-        resource.afterInsert.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, component, 'CREATE', resource));
-        resource.afterUpdate.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, component, 'UPDATE', resource));
-        resource.afterDelete.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, component, 'DELETE', resource));
+        resource.afterInsert.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, 'CREATE', resource));
+        resource.afterUpdate.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, 'UPDATE', resource));
+        resource.afterDelete.push((tx, sriRequest, elements) => doAudit(tx, pluginConfig, sriRequest, elements, 'DELETE', resource));
       });
-    }
-  };
-
+  }
 };
